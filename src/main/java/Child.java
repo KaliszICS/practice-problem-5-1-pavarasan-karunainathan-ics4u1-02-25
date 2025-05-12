8e55114733b2cbc5914a56a62db97d5dba42f6f1
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-05-12 13:49:56 +0000

    Problems Complete?

---
+++ src/main/java/Child.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Child extends Person{
+    Parent parent1;
+    Parent parent2;
+    Child[] siblings;
+
+    public Child(String name, int age, Parent parent1, Parent parent2){
+        super(name, age);
+        this.parent1 = parent1;
+        this.parent2 = parent2;
+        siblings = new Child[0];
+    }
+
+    public Parent getParent1(){return this.parent1;}
+    public Parent getParent2(){return this.parent2;}
+    public Child[] getSiblings(){return this.siblings;}
+    public void setSiblings(Child[] siblings){
+        this.siblings = siblings;
+    }
+
+    public void addSibling(Child child){
+        this.siblings = Arrays.copyOf(this.siblings, this.siblings.length+1);
+        this.siblings[this.siblings.length-1] = child;
+    }
+}
---
+++ src/main/java/Parent.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Parent extends Person{
+    Parent spouse;
+    Child[] children;
+
+    public Parent(String name, int age, Parent spouse){
+        super(name, age);
+        this.spouse = spouse;
+        this.children = new Child[0];
+    }
+
+    public Parent(String name, int age){
+        super(name, age);
+        spouse = null;
+        this.children = new Child[0];
+    }
+
+    public Parent getSpouse(){return this.spouse;}
+    public Child[] getChildren(){return this.children;}
+    public void setSpouse(Parent spouse){this.spouse = spouse;}
+    public void setChildren(Child[] children){
+        this.children = children;
+    }
+
+    public void addChild(Child child){
+        this.children = Arrays.copyOf(this.children, this.children.length+1);
+        this.children[this.children.length-1] = child;
+    }
+}
